bb5e251eb6a5fcf775a2a97cd166f0408cf5912b
MANIKANTARAMKUMAR <manikantaramkumar760@gmail.com>
2025-12-30 19:34:08 +0530

    save new files

--- Class Work/1.amstrong number.py
+++ Class Work/1.amstrong number.py
@@ -1,4 +1,4 @@
-a="37"
+"""a="37"
 sum=0
 l=len(a)
 for i in a :
@@ -10,3 +10,33 @@ else:
 
 
 
+"""
+"""
+num = int(input("Enter a number: "))
+for i in range (num):
+    for j in range (i):
+        print(end=" ")
+print("#",end=" ")"""
+
+"""n = int(input("Enter a number: "))
+for i in range (n):
+    if i<=n//2:
+        print("* "*(i+1))
+    else:
+        print("* " *(n-i))
+
+"""
+'''A=int(input("Enter a number: "))
+for i in range (A):
+    for j in range (A):
+        if i==0 or i==A//2 or j==0 or j==A-1 or i==A-1:
+            print("* ",end=" ")
+        else:
+            print(" ",end=" ")
+            '''
+a=int(input("enter a number:"))
+for i in range (a):
+    for j in range (a):
+        if i==0 or j==0 or i==a-1 or j==a:
+            print("* ",end=" ")
+        print()
