b5a22a11d069d15064c276e362ad018c317c063b
MANIKANTARAMKUMAR <manikantaramkumar760@gmail.com>
2026-02-18 10:55:41 +0530

    save new files

--- Class Work/oopsrevision.py
+++ Class Work/oopsrevision.py
@@ -57,3 +57,100 @@ a.withdraw(800)
 
 
 '''
+
+class car:
+    def __init__(self,brand,model,year):
+        self.brand=brand
+    def start (self):
+        print(f"{self.brand}  is starting")
+class vehicle(car):
+    def __init__(self,brand,model,year):
+        super().__init__(brand,model,year)
+    def start(self):
+        print(f"{self.brand} is starting with a roar")
+    def show_details(self):
+        print(f"brand: {self.brand} ")
+
+a=vehicle("ford","mustang",2020)
+a.start()
+a.show_details()
+b=car("toyota","camry",2019)
+b.start()
+
+
+
+class Account:
+    def __init__(self):
+       self.__balance=0
+    def deposit(self,amount):
+        self.__balance+=amount
+        print(self.__balance)
+    def withdraw(self,amount):
+        if self.__balance>=amount:
+            self.__balance-=amount
+            print(self.__balance)
+        else:
+            print("insufficient balance")
+a=Account()
+a.deposit(1000)
+a.withdraw(210)
+
+from abc import ABC,abstractmethod
+class payment(ABC):
+    @abstractmethod
+    def pay(self,amount):
+        self.amount=amount
+        print(f"paying {self.amount}")
+        
+    def UPI(self,amount):
+        self.amount=amount
+        print(f"paying {self.amount} through UPI") 
+class CreditCard(payment):
+    def pay(self,amount):
+        super().pay(amount)
+    def UPI(self,amount):
+        super().UPI(amount)
+a=CreditCard()
+a.pay(1000)
+a.UPI(500)
+
+
+class notification:
+    @abstractmethod
+    def notification(self):
+        pass
+class email(notification):
+    def notify(self):
+        print("sending email notification")
+class sms(notification):
+    def notify(self):
+        print("sending sms notification")
+a=email()
+a.notify()  
+b=sms()
+b.notify()
+
+class transportation:
+    def __init__(self,mode):
+        self.mode=mode
+    def travel(self):
+        print(f"traveling by {self.mode}")
+
+
+class employee:
+    def developper(self):
+        print("developing software")
+class manager(employee):
+    def manage(self):
+        super().developper()
+        print("managing team")     
+class testing(employee):
+    def test(self):
+        super().developper()
+        print("testing software") 
+
+a=manager()
+a.manage()
+b=testing()
+b.test()    
+
