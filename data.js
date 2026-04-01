c2955b0ae2a9a84fa30b97c68f0bf7304d67933b
MANIKANTARAMKUMAR <manikantaramkumar760@gmail.com>
2026-04-01 11:58:36 +0530

    js files

--- data.js
+++ data.js
@@ -5,3 +5,54 @@ var b=10;
 console.log(num);
 console.log(a);
 console.log(b);
+
+
+let add =a+b;
+console.log(add);
+let sub =a-b;
+console.log(sub);
+let mul =a*b;   
+console.log(mul);
+let div =a/b;
+console.log(div);
+
+let e =15;
+e*=2;
+console.log(e);
+
+let f =10;
+f/=2;
+console.log(f);
+
+let g =5;
+g+=3;
+console.log(g);
+
+
+let valu_1 = 10>10;
+console.log(valu_1);
+
+let valu_2 = 10<10;
+console.log(valu_2);        
+
+let valu_3 = 10>=10;
+console.log(valu_3);
+
+let valu_4 = 10<=10;
+console.log(valu_4);
+
+let valu_5 = 10==10;
+console.log(valu_5);
+
+let result = 10!=10;
+console.log(result);
+
+
+let h = 5=='5';
+console.log(h);
+
+let i = 5==='5';
+console.log(i);
+
+let m =5!=='5';
+console.log(m);
